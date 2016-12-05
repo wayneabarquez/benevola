1f7384918e2c986eef0116cec43e402d0048db78
Your Name <you@example.com>
2016-12-05 11:10:11 +0800

    fix sorting of lots on lot list report

--- app/services/reports_service.py
+++ app/services/reports_service.py
@@ -8,6 +8,7 @@ from app.services import section_service
 import logging
 import datetime
 from operator import itemgetter
+import re
 
 log = logging.getLogger(__name__)
 
@@ -125,7 +126,7 @@ def print_lot_list_sections_data(ws):
         # TABLE BODY - LOTS
         # sorted_blocks = sorted(section.blocks, key=lambda block: block.name)
         for b in section.blocks:
-            sorted_lots = sorted(b.lots, key=lambda lot: lot.name)
+            sorted_lots = sorted(b.lots, key=lambda lot: int(re.sub('[^0-9]', '', lot.name)))
             for l in sorted_lots:
                 client_name = l.client.get_full_name() if l.client is not None else ''
                 price_formatted = '{:20,.2f}'.format(l.price_per_sq_mtr)
